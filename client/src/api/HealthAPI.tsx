99fd94aede983aa5ba2afe0b27ab081250cec11f
Adrian Iurian <adrian.iurian1@gmail.com>
2024-10-27 04:30:03 +0200

    switch to fetch

--- client/src/api/HealthAPI.tsx
+++ client/src/api/HealthAPI.tsx
@@ -1,29 +1,40 @@
-import { useContext } from "react";
-import UserContext from "../store/UserContext.tsx";
+// import { useContext } from "react";
+// import UserContext from "../store/UserContext.tsx";
 
 const BACKEND_URL = "https://docmed.ccrolabs.com";
 
 const useAPI = () => {
-    const { axiosAPI } = useContext(UserContext);
+    // const { axiosAPI } = useContext(UserContext);
+
+    // const postQuestions1 = async (symptoms: string) => {
+    //     try {
+    //         const response = await axiosAPI.post(
+    //             `${BACKEND_URL}/api/prompting/questions`,
+    //             {
+    //                 symptoms: symptoms,
+    //             },
+    //             {
+    //                 headers: {
+    //                     "Content-Type": "application/json",
+    //                 },
+    //             }
+    //         );
+    //         return response.data;
+    //     } catch (error) {
+    //         console.error("Error fetching health status:", error);
+    //         throw error;
+    //     }
+    // };
 
     const postQuestions = async (symptoms: string) => {
-        try {
-            const response = await axiosAPI.post(
-                `${BACKEND_URL}/api/prompting/questions`,
-                {
-                    symptoms: symptoms,
-                },
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            );
-            return response.data;
-        } catch (error) {
-            console.error("Error fetching health status:", error);
-            throw error;
-        }
+        const response = await fetch(`${BACKEND_URL}/api/prompting/questions`, {
+            method: "POST",
+            body: JSON.stringify({ symptoms: symptoms }),
+            headers: { "Content-Type": "application/json" },
+        });
+        const data = await response.json();
+        console.log(data);
+        return data;
     };
 
     return {
