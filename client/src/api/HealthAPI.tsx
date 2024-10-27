27210f2213da69442971cc1de44f2f4e47105112
Adrian Iurian <adrian.iurian1@gmail.com>
2024-10-27 03:38:28 +0300

    add questions post

---
+++ client/src/api/HealthAPI.tsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import UserContext from "../store/UserContext.tsx";
+
+const BACKEND_URL =
+    "https://hacktech-backend-296479925771.europe-west4.run.app";
+
+const useAPI = () => {
+    const { axiosAPI } = useContext(UserContext);
+
+    const postQuestions = async (symptoms: string) => {
+        try {
+            const response = await axiosAPI.get(
+                `${BACKEND_URL}/api/prompting/questions`,
+                {
+                    data: {
+                        symptoms: symptoms,
+                    },
+                }
+            );
+            return response.data;
+        } catch (error) {
+            console.error("Error fetching health status:", error);
+            throw error;
+        }
+    };
+
+    return {
+        postQuestions,
+    };
+};
+
+export default useAPI;
--- client/src/pages/Request.tsx
+++ client/src/pages/Request.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Input from "../components/UI/Input";
 import Button from "../components/UI/Button";
 import FormCard from "../components/FormCard";
+import useAPI from "../api/HealthAPI";
 
 type QuestionType = {
     question: string;
@@ -21,34 +22,42 @@ export default function RequestPage() {
     const [questions, setQuestions] = useState([]);
     const [medication, setMedication] = useState("");
 
-    return (
-        <div className="flex flex-col gap-6 px-6">
-            <FormCard title="Document section">File input</FormCard>
+    const { postQuestions } = useAPI();
 
-            <div className="flex flex-row gap-6">
+    return (
+        <div className='flex flex-col gap-6 px-6'>
+            <FormCard title='Document section'>File input</FormCard>
+
+            <div className='flex flex-row gap-6'>
                 <FormCard
-                    title="Symptoms section"
-                    className="flex-1 flex flex-col"
+                    title='Symptoms section'
+                    className='flex-1 flex flex-col'
                 >
                     <Input
-                        id="base-symptom"
-                        label="What are your symptoms?"
+                        id='base-symptom'
+                        label='What are your symptoms?'
                         value={baseSymptom}
                         onChange={(value) => setBaseSymptom(value as string)}
                     />
 
-                    <div className="flex flex-row justify-between items-center py-4">
-                        <label className="text-sm font-semibold text-stone-400">
+                    <div className='flex flex-row justify-between items-center py-4'>
+                        <label className='text-sm font-semibold text-stone-400'>
                             The quality of your answers will determine the
                             accuracy of the results.
                         </label>
-                        <Button onClick={() => {}} outlined styleType="success">
+                        <Button
+                            onClick={() => {
+                                postQuestions(baseSymptom);
+                            }}
+                            outlined
+                            styleType='success'
+                        >
                             Send
                         </Button>
                     </div>
 
                     {questions.length === 0 ? (
-                        <div className="border border-stone-400 text-stone-400 rounded-lg flex justify-center items-center flex-1">
+                        <div className='border border-stone-400 text-stone-400 rounded-lg flex justify-center items-center flex-1'>
                             Please enter your symptoms in order to get follow-up
                             questions.
                         </div>
@@ -61,23 +70,23 @@ export default function RequestPage() {
                     )}
                 </FormCard>
 
-                <div className="flex flex-col flex-1 gap-6">
-                    <FormCard title="Medication section">
+                <div className='flex flex-col flex-1 gap-6'>
+                    <FormCard title='Medication section'>
                         <Input
                             value={medication}
                             onChange={(value) => setMedication(value as string)}
-                            id="medication"
-                            label="Medication"
+                            id='medication'
+                            label='Medication'
                             textarea
                             rows={4}
                         />
 
-                        <label className="text-sm font-semibold text-stone-400">
+                        <label className='text-sm font-semibold text-stone-400'>
                             Please enter your current medication treatment
                         </label>
                     </FormCard>
 
-                    <FormCard title="Context">Context</FormCard>
+                    <FormCard title='Context'>Context</FormCard>
                 </div>
             </div>
         </div>
