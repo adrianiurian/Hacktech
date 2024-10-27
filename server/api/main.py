129c622d9a9de8a4e12354ba1827695407610be7
Razvi103 <bocrar@gmail.com>
2024-10-27 05:18:41 +0200

    added routes to main

--- server/api/main.py
+++ server/api/main.py
@@ -26,11 +26,31 @@ def make_app():
 
     from api.routes import prompt
     from api.routes import auth
+    from api.routes import context
     from api.routes import file
+    from api.routes import final_prompt
+    from api.routes import get_file
+    from api.routes import google_fit
+    from api.routes import health_report
+    from api.routes import medication
+
+
+
+
+
 
     _app.include_router(prompt.router)
     _app.include_router(auth.router)
     _app.include_router(file.router)
+    _app.include_router(context.router)
+    _app.include_router(final_prompt.router)
+    _app.include_router(get_file.router)
+    _app.include_router(google_fit.router)
+    _app.include_router(health_report.router)
+    _app.include_router(medication.router)
+
+
+
 
     _app.add_middleware(
         CORSMiddleware,
